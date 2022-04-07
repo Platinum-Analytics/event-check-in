3871cbc724fba1ca5c48e74fc402752cf0a4a60a
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-04-07 00:09:12 -0400

    Modified wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from eventCheckIn.__init__ import create_app
+from eventCheckIn import create_app
 
 app_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, app_path)
