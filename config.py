bbe3631781b4af2c21738dda96272b3fa6836609
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2022-04-09 00:03:32 -0400

    Renamed config var for database

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY")  # same key will be used for csrf protection
 
     # database
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")  # sqlite
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # sqlite
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
