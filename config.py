9607b8b5bf77bb9131c91f33b439d8de9229f426
Arch-it-12 <41963542+Arch-it-12@users.noreply.github.com>
2023-05-09 23:36:47 -0400

    Update requirements and change to sqlite

--- config.py
+++ config.py
@@ -14,9 +14,7 @@ class Config(object):
     PERMANENT_SESSION_LIFETIME = timedelta(hours=12)
 
     # flask-SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
 
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
